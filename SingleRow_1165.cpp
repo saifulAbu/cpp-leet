8e04afea6c08c3142c136d88adcd36d66303a60d
Saif Abu <saifabu@microsoft.com>
2022-07-11 18:11:44 -0700

    add more stuffs

--- SingleRow_1165.cpp
+++ SingleRow_1165.cpp
@@ -2,13 +2,26 @@
 #include <vector>
 #include <iostream>
 #include <map>
+#include <cstdlib>
 
 using namespace std;
 
 class Solution {
 public:
     int calculateTime(string keyboard, string word) {
-        map
+        map<char, int> kmap;
+        for(int i = 0; i < keyboard.size(); i++) {
+            kmap[keyboard[i]] = i;
+        }
+        
+        int prev = 0;
+        int res = 0;
+        for(int i = 0; i < word.size(); i++) {
+            int cur = kmap[word[i]];
+            res += abs(cur - prev);
+            prev = cur;
+        }
+        return res;
     }
 };
 
