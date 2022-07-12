4131fd61b0c7a9c95aa05063e634abaa6e6674f1
Saif Abu <saifabu@microsoft.com>
2022-07-11 18:11:19 -0700

    add more stuffs

---
+++ SingleRow_1165.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+    int calculateTime(string keyboard, string word) {
+        map
+    }
+};
+
